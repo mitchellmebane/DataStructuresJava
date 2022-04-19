abc68c942ffafa40e970e80773eadf956fb24673
Mitchell Mebane <mitchell.mebane@gmail.com>
2022-04-18 23:34:27 -0500

    Remove unused import

--- app/src/main/java/com/mitchellmebane/linkedlist/LinkedList.java
+++ app/src/main/java/com/mitchellmebane/linkedlist/LinkedList.java
@@ -2,7 +2,6 @@ package com.mitchellmebane.linkedlist;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.function.Function;
 import java.util.stream.Collector;
 
 public class LinkedList<T> implements Iterable<T> {
